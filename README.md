e6897205dac76f62cb962e90009ce55be98ec1ad
Timothy Johnson <timjohnson1897@gmail.com>
2023-01-18 15:16:23 -0600

    did a little organization and filled out the README

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # calendar-app
+
+## Description
+
+- This is a simple app that lets you plan and save your day within 1-hour time blocks
+
+- The current date is displayed a the top of the screen in (Day of the Week, Month, Day) format
+
+- The different colors of the boxes represent the past, present, and the future. Grey = past, Red = present, and Green = future.
+
+## Installation
+
+URL GOES HERE
+
+## Usage
+
+- Click on the fields with the correspoding time slot and type what you have planned within.
+
+- When you are finished typing click on the save icon left adjacent to the box you are typing in, This will save your entry even if you close the page.
+
+## Notes
+
+- This weeks assignment was a pretty hard challenge for me, I am slow to learn new syntax but I'm glad I think I got it down enough to work
+
+- Towards the end of the assignment I figured out much easier ways to select the data I wanted, I did the save button first and I used a pretty roundabout way of navigating the DOM
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 $(document).ready(function () {
  var saveBtn = $(".saveBtn");
 
+ //call to display the saved plans
  renderLocalStorage();
 
+ //Modifies the classes of elements that share the .time-block class to change the color of the time slot
  $(".time-block").each(function(){
+   //checks if the time-slot is the current time
    if(parseInt($(this).attr("id")) == dayjs().format('H'))
    {
       $(this).removeClass("future");
       $(this).removeClass("past");
       $(this).addClass("present");
    }
-
+   //checks if the time slot is after the current time slot
    else if(parseInt($(this).attr("id")) > dayjs().format('H'))
    {
       $(this).removeClass("present");
       $(this).removeClass("past");
       $(this).addClass("future");
    }
-
+   //else the time slot must be in the past
    else
    {
       $(this).removeClass("present");
@@ -32,29 +32,14 @@ $(document).ready(function () {
 
  renderLocalStorage();
 
+ //When the save icon is clicked the information in the textarea is saved in local storage using the ID as a key
  saveBtn.on('click',function(){
     var toDoText = this.parentElement.children[1].value;
     localStorage.setItem(this.parentElement.id,toDoText);
  });
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+      //places the value stored in the local storage and places it in the corresponding time slot's text area.
      function renderLocalStorage(){
-      $(".time-block").each(function(){
          $("#09 textarea").val(localStorage.getItem("09"));
          $("#10 textarea").val(localStorage.getItem("10"));
          $("#11 textarea").val(localStorage.getItem("11"));
@@ -64,9 +49,8 @@ $(document).ready(function () {
          $("#15 textarea").val(localStorage.getItem("15"));
          $("#16 textarea").val(localStorage.getItem("16"));
          $("#17 textarea").val(localStorage.getItem("17"));
-     })
    }
-  // TODO: Add code to display the current date in the header of the page.
+  // Displays the current date at the top of the page
   var dateEl = $('#currentDay');
   dateEl.text(dayjs().format('dddd, MMMM DD'));
 });
